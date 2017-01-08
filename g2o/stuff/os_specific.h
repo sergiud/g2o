4c4beca50d1c0994a217024f762e699643849447
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2017-01-08 20:38:37 +0100

    do not use .c extension for source files
    This avoids warnings from the Intel compiler which tries to apply C++ flags to C source files in mixed targets.

--- g2o/stuff/os_specific.h
+++ g2o/stuff/os_specific.h
@@ -28,23 +28,15 @@
 #define G2O_OS_SPECIFIC_HH_
 
 #ifdef WINDOWS
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdarg>
 #ifndef _WINDOWS
 #include <sys/time.h>
 #endif
 #define drand48() ((double) rand()/(double)RAND_MAX)
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int vasprintf(char** strp, const char* fmt, va_list ap);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
 
